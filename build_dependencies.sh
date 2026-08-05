7755f3ba20506e041a2bdcdd885fa91e4d2b416a
srinibas15 <113517102+srinibas15@users.noreply.github.com>
2026-08-05 18:18:42 +0530

    RDKEMW-22169 : use 4.0.8 tag of entservices-apis (#20)
    Co-authored-by: smohap466_comcast <srinibas_mohapatra@comcast.com>

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -49,7 +49,7 @@ git clone --branch  R4.4.3 https://github.com/rdkcentral/ThunderTools.git
 
 git clone --branch R4.4.1 https://github.com/rdkcentral/Thunder.git
 
-git clone --branch develop https://github.com/rdkcentral/entservices-apis.git
+git clone --branch 4.0.8 https://github.com/rdkcentral/entservices-apis.git
 
 cd ..
 git clone --branch develop https://github.com/rdkcentral/entservices-helpers.git
