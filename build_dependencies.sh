0072590a25c7bcb63fb036b3f4be1da7c32dd4cd
smohap466_comcast <srinibas_mohapatra@comcast.com>
2026-08-07 19:18:04 +0530

    RDKEMW-22169: Fixing L2 WorkFlow Failure

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -63,7 +63,7 @@ cd ..
 git clone --branch develop https://github.com/rdkcentral/entservices-helpers.git
 cd "$GITHUB_WORKSPACE"
 
-git clone --branch feature/RDKEMW-22169 https://github.com/rdkcentral/entservices-testframework.git
+git clone --branch develop https://github.com/rdkcentral/entservices-testframework.git
 
 ############################
 # Build Thunder-Tools
