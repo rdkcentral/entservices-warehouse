ba665e2ab04f85addcdd99d1f8cc3afbda4d7f44
srinibas15 <113517102+srinibas15@users.noreply.github.com>
2026-08-10 15:15:56 +0530

    RDKEMW-22169:Update build_dependencies.sh

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -47,23 +47,23 @@ cd ..
 # Clone the required repositories
 
 
-git clone -b R4_4-RDK https://github.com/rdkcentral/ThunderTools.git
+git clone --branch R4_4-RDK https://github.com/rdkcentral/ThunderTools.git
 cd ThunderTools
 git checkout $THUNDER_TOOLS_COMMIT_SHA
 cd ..
 
-git clone -b R4_4-RDK https://github.com/rdkcentral/Thunder.git
+git clone --branch R4_4-RDK https://github.com/rdkcentral/Thunder.git
 cd Thunder
 git checkout $THUNDER_COMMIT_SHA
 cd ..
 
-git clone --branch 4.0.8 https://github.com/rdkcentral/entservices-apis.git
+git clone --branch develop https://github.com/rdkcentral/entservices-apis.git
 
 cd ..
 git clone --branch develop https://github.com/rdkcentral/entservices-helpers.git
 cd "$GITHUB_WORKSPACE"
 
-git clone --branch develop https://github.com/rdkcentral/entservices-testframework.git
+git clone --branch 2.0.0 https://github.com/rdkcentral/entservices-testframework.git
 
 ############################
 # Build Thunder-Tools
